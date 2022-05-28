5591fa6e2cb48b5a3bd35650e6d4865d775abdf8
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-28 21:28:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const diceOne = document.querySelector(`.dice-one`);
+const diceTwo = document.querySelector(`.dice-two`);
+const diceThree = document.querySelector(`.dice-three`);
+const diceFour = document.querySelector(`.dice-four`);
+const scorePlayerOne = document.querySelector(`.score-player1`);
+const scorePlayerTwo = document.querySelector(`.score-player2`);
+const diceScorePlayerOne = document.querySelector(`.dice-score-player1`);
+const diceScorePlayerTwo = document.querySelector(`.dice-score-player2`);
+const buttonRollDicePlayerOne = document.querySelector(`.roll-dice-player1`);
+const buttonRollDicePlayerTwo = document.querySelector(`roll-dice-player2`);
+const buttonPlayAgaing = document.querySelector(`.play-again`);
+const buttonNewGame = document.querySelector(`.new-game`);
+const dipsplayWinner = document.querySelector(`.winner`);
